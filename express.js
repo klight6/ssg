f91b91cb43436578a25945640e098bb6f1d12950
klight6 <klight6@naver.com>
2024-07-02 21:29:01 +0900

    커밋 내용

--- express.js
+++ express.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
-const { findUser, users } = require('./user');
-const cookieParser = require('cookie-parser'); 
+const cookieParser = require('cookie-parser');
+const userRoutes = require('./router');
+
 const app = express();
 const port = 3000;
 
@@ -19,68 +20,11 @@ nunjucks.configure("./views", {
 
 app.use('/', express.static("./public"));
 
+app.use('/user', userRoutes);
+
 app.get('/', (req, res) => {
   res.setHeader("Set-Cookie", "token=1");
   res.render("index3.html");
-});
-
-app.get('/login', (req, res) => {
-  if (req.headers.cookie) {
-    let data = req.headers.cookie;
-    let sessionId = data.split('=')[1];
-    res.render('index_login.html', {
-      sessionId
-    });
-  } else {
-    res.render("index4.html");
-  }
-});
-
-app.get("/user/login", (req, res) => {
-  let msg = req.query.msg;
-  res.render('./user/login.html', {
-    msg
-  });
-});
-
-app.post("/user/login", (req, res) => {
-  let { userid, userpw } = req.body;
-
-  let loginFlag = findUser(userid, userpw);
-  if (loginFlag) {
-    const privateKey = parseInt(Math.random() * 10000000000).toString();
-    const item = users.find(v => v.userid === userid);
-    session[privateKey] = item;
-
-    res.setHeader('Set-Cookie', `connect.id=${privateKey}; path=/`);
-    res.redirect('/login');
-  } else {
-    res.redirect('/user/login?msg=아이디와 패스워드가 일치하지 않습니다.');
-  }
-});
-
-app.get('/user/profile', (req, res) => {
-  let sessionId = req.cookies['connect.id'];
-  let userData = session[sessionId];
-
-  console.log(userData);
-
-  if (sessionId) {
-    res.render('./user/profile.html', {
-      userData
-    });
-  }
-});
-
-
-app.get('/cookie', (req, res) => {
-  let cookie = req.headers.cookie;
-
-  if (cookie === undefined) {
-    res.send("<h1>There is no cookie</h1>");
-  } else {
-    res.send(`<h1>Cookie: ${cookie}</h1>`);
-  }
 });
 
 app.get('/hello', (req, res) => {
